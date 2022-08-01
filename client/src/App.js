d8e0c5dc883953147748490460d01bc6a3cb0b46
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-08-01 23:25:48 +0300

    create initaial architecture and page components

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-2xl text-red-600">hello</div>;
+}
+
+export default App;
---
+++ client/src/pages/addPostPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPostPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AddPostPage;
