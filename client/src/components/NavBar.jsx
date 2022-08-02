e8cd91ad9c7fbcb0b7ce54e7bf97ca961c12d434
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-08-02 10:37:35 +0300

    create LoginPage and Registerpage

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,8 +10,9 @@ export const NavBar = () => {
 
   return (
     <div className="flex py-4 justify-between items-center">
-      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
-        A
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600
+       text-xs text-white rounded-sm">
+        B
       </span>
 
       {isAuth && (
